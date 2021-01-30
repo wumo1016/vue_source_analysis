b945d66982ae7672f7fae6744990769dc37e2196
wumo1016 <760478279@qq.com>
2021-01-30 20:13:03 +0800

    update

--- src/test/src/App.vue
+++ src/test/src/App.vue
@@ -1,25 +1,17 @@
 <template>
   <div>
-    {{ obj.age }}
-    <button @click="test">test</button>
   </div>
 </template>
 <script>
-import Headers from '@/components/headers'
+// import Headers from '@/components/headers'
 export default {
   // components: {Headers},
   name: "App",
   data(){
     return {
-      obj: {
-        name: 123
-      }
     }
   },
   methods: {
-    test(){
-      this.$set(this.obj, 'age', 456)
-    }
   }
 };
 </script>
